79525e571da67bc3066f4234426cab363b7d266a
Caroline Breitenbach <carolinebreitenbach@gmail.com>
2022-05-24 15:47:23 -0300

    Classe user resource findById

--- src/main/java/br/com/carol/api/resources/UserResource.java
+++ src/main/java/br/com/carol/api/resources/UserResource.java
@@ -2,6 +2,8 @@ package br.com.carol.api.resources;
 
 
 import br.com.carol.api.domain.User;
+import br.com.carol.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
+
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-      return ResponseEntity.ok().body(new User(1,"Carol","carolinebreitenbach@gmail.com","123"));
+      return ResponseEntity.ok().body(service.findById(id));
 
     }
 
