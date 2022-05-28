d220729f7f5e7f5dc4f320639ffd3e0b57ee0ebe
Caroline Breitenbach <carolinebreitenbach@gmail.com>
2022-05-28 18:20:34 -0300

    teste para classe user service metodo findById objeto nao encontrado

--- src/test/java/br/com/carol/api/services/Impl/UserServiceImplTest.java
+++ src/test/java/br/com/carol/api/services/Impl/UserServiceImplTest.java
@@ -28,6 +28,7 @@ class UserServiceImplTest {
     public static final String NAME = "valdir";
     public static final String EMAIL = "valdir@gmail.com";
     public static final String PASSWORD = "123";
+    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
     @InjectMocks
     private UserServiceImpl service;
     @Mock
@@ -59,12 +60,12 @@ class UserServiceImplTest {
 
     @Test
     void whenFindByIdThenReturnAndObjectNotFoundException() {
-        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException("Objeto não encontrado"));
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
         try {
             service.findById(ID);
         } catch (Exception ex) {
             assertEquals(ObjectNotFoundException.class, ex.getClass());
-            assertEquals("Objeto não encontrado", ex.getMessage());
+            assertEquals(OBJETO_NAO_ENCONTRADO, ex.getMessage());
         }
     }
 
